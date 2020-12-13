b31ceb86d3720661f3425dfa1aa2ee358f64e8f3
毛毛猿 <mmao6930@gmail.com>
2020-12-13 00:06:31 -0500

    登录完成

---
+++ src/components/Login.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="login">
+    <div class="login_box">
+      <div class="header_img">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <el-form :model="login_form"
+      :rules="rules" label-width="0px" ref="loginRef" class="login_form">
+        <el-form-item prop="username">
+          <el-input v-model="login_form.username" prefix-icon="el-icon-user-solid"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="login_form.password" prefix-icon="el-icon-lock" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="reset">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      login_form: {
+        username: 'admin',
+        password: 'admin'
+      },
+      // 表单验证规则
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 重置
+    reset () {
+      this.$refs.loginRef.resetFields()
+    },
+    login () {
+      this.$refs.loginRef.validate(valid => {
+        if (!valid) return this.$message.error('登入失败')
+        this.$message.success('登入成功')
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  background-color: #ffdb00;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.header_img {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+}
+.header_img img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #eee;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import './assets/css/globel.css'
+
+import axios from 'axios'
+// axios.defaults.baseURL = 'http://'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
